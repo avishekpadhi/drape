2ac7e78d9c9210ec4426b610f9915eebd50841aa
Abhishek Padhi <abhishekpadhi69@gmail.com>
2025-06-20 15:12:53 +0530

    chore: fixed db connection in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import productRouter from "../src/routes/productRoutes";
-
+import { connectDB } from "./config/db";
+import dotenv from "dotenv";
 const app = express();
 const PORT = 3001;
 
+dotenv.config();
+
 app.use(cors());
 app.use("/products", productRouter);
+
+connectDB();
 
 app.use((req, res) => {
   console.log("Fallback route hit");
