c56e2b2d4f564890bf95b40d026eabbcf27bee9a
Abhishek Padhi <abhishekpadhi69@gmail.com>
2025-08-27 11:56:57 +0530

    feat:stacked the thumbnail grid to the left of the main image in product detail page

--- frontend/src/pages/Productdetails/ImageContainer.tsx
+++ frontend/src/pages/Productdetails/ImageContainer.tsx
@@ -20,28 +20,28 @@ const ImageContainer = ({ product }: ImageContainerProps) => {
 
   return (
     <div className="flex gap-6">
-      <div className="flex flex-col gap-4 w-1/2">
-        {/* Main Image */}
-        <div className="w-full">
-          <img
-            className="w-full aspect-[3/4] object-cover object-center rounded-lg"
-            src={selectedImage || ""}
-            alt="main product"
-          />
-        </div>
-
+      <div className="flex flex-row w-2/5 items-start">
         {/* Thumbnail Grid */}
-        <div className="grid grid-cols-5 gap-4">
+        <div className="flex flex-col gap-4 w-1/5 mr-4">
           {product.images?.map((img, idx) => (
             <img
               key={idx}
               src={img}
               onClick={() => setSelectedImage(img)}
-              className={`object-cover object-center h-20 w-full rounded-lg cursor-pointer transition
+              className={`object-cover object-center h-16 w-full rounded-lg cursor-pointer transition
                 ${selectedImage === img ? "ring-2 ring-blue-500" : ""}`}
               alt={`thumbnail-${idx}`}
             />
           ))}
+        </div>
+
+        {/* Main Image */}
+        <div className="w-4/5">
+          <img
+            className="w-full aspect-[3/4] object-cover object-center rounded-lg"
+            src={selectedImage || ""}
+            alt="main product"
+          />
         </div>
       </div>
     </div>
