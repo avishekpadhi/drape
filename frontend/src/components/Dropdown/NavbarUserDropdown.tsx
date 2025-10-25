ccecf81e3ec599f2377711be8cb3b481c7d44884
Abhishek Padhi <abhishekpadhi69@gmail.com>
2025-10-25 22:19:27 +0530

    feat: implemented logout

--- frontend/src/components/Dropdown/NavbarUserDropdown.tsx
+++ frontend/src/components/Dropdown/NavbarUserDropdown.tsx
@@ -1,29 +1,43 @@
 import { useContext } from "react";
 import { Dropdown } from "../ui/Dropdown";
-import { LogOut, User } from "lucide-react";
+import { LogOut, User, LogIn } from "lucide-react";
 import { AuthContext } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
 
 export const NavbarUserDropdown = () => {
   const { currentUser, setCurrentUser } = useContext(AuthContext);
   const navigate = useNavigate();
+
   const handleLogout = () => {
     setCurrentUser(null);
     localStorage.clear();
     navigate("/login");
   };
-  console.log(currentUser);
 
-  return (
-    <Dropdown
-      trigger={<User size={30} className="cursor-pointer" />}
-      items={[
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const items = currentUser
+    ? [
         {
           label: "Log Out",
           icon: <LogOut size={16} />,
           onClick: handleLogout,
         },
-      ]}
+      ]
+    : [
+        {
+          label: "Login",
+          icon: <LogIn size={16} />,
+          onClick: handleLogin,
+        },
+      ];
+
+  return (
+    <Dropdown
+      trigger={<User size={30} className="cursor-pointer" />}
+      items={items}
     />
   );
 };
