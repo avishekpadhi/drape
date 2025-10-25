3f6bf230ee161d0d2ff415cfdc3b71b2d6f45401
Abhishek Padhi <abhishekpadhi69@gmail.com>
2025-10-25 14:00:34 +0530

    feat: added shadcn's dropdown menu

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { Dropdown } from "../Dropdown/Dropdown";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-14 bg-white shadow z-50 flex items-center px-6">
       <Link to="/products">Drape</Link>
+      <div className="flex-grow"></div>
+      <Dropdown />
     </div>
   );
 };
