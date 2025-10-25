56eda424b63be6c775ba05173c32a70eeff4e77c
Abhishek Padhi <abhishekpadhi69@gmail.com>
2025-10-25 19:58:08 +0530

    chore: added the logout menu in the navbar

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -3,21 +3,21 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
 
 export function Dropdown() {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuTrigger>
+        <User size={30} className="cursor-pointer" />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-48 bg-background shadow-lg rounded-lg mr-2">
+        <DropdownMenuItem className="flex items-center gap-2 px-3 py-2 hover:bg-border cursor-pointer">
+          <LogOut size={16} /> Log Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
